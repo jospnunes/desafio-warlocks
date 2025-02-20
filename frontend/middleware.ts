cfb1e93506be7dde2c755a8f9e1c47043358a9f2
José Pedro Nunes Pereira <Jospnunes@gmail.com>
2025-02-20 00:07:12 -0300

    Atualizar o middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,11 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // Tenta obter o token do cookie "token"
+  
   const token = request.cookies.get("token")?.value;
   console.log("Token from NextRequest.cookies:", token);
-
-  // Se não houver token, redireciona para a página de login
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url), 307);
   }
